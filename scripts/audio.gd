172b2b895961e7cfbe4af5749b2b74b0373ae803
Igor <igorscorched@gmail.com>
2023-11-02 15:10:28 +0400

    FMOD integration, except the addons folder.

---
+++ scripts/audio.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@export var event: EventAsset
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	FMODRuntime.play_one_shot(event)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
