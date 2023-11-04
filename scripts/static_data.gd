b27f42f56fe6a83fdcc74448c45f4a903ef6f581
Igor <igorscorched@gmail.com>
2023-11-05 03:01:48 +0400

    Changing unit parameters now updates visuals in the editor.

--- scripts/static_data.gd
+++ scripts/static_data.gd
@@ -2,3 +2,16 @@ extends Node
 
 @export var color_pink: Color = Color.PINK
 @export var color_blue: Color = Color.BLUE
+@export var color_neutral: Color = Color.GRAY
+
+enum UnitTypes { CENTRAL_NODE, TOWER_NODE, WORM, TROJAN, VIRUS }
+enum HackingGroups { PINK, BLUE, NEUTRAL }
+
+@export var unit_stats = { 
+	UnitTypes.CENTRAL_NODE: { "hp_max": 100, "ap_max": 10 },
+	UnitTypes.TOWER_NODE: { "hp_max": 100, "ap_max": 10, "attack": 10, "attack_extra": 5 },
+	
+	UnitTypes.WORM: { "hp_max": 5, "ap_max": 10 },
+	UnitTypes.TROJAN: { "hp_max": 12, "ap_max": 10 },
+	UnitTypes.VIRUS: { "hp_max": 30, "ap_max": 10, "attack": 3, "attack_extra": 2 },
+	}
--- scripts/unit.gd
+++ scripts/unit.gd
@@ -1,10 +1,18 @@
+@tool
 extends Node3D
 
-enum Types { CENTRAL_NODE, TOWER_NODE, WORM, TROJAN, VIRUS }
-enum HackingGroups { PINK, BLUE, NEUTRAL }
+@export var type: StaticData.UnitTypes = StaticData.UnitTypes.TOWER_NODE:
+	set(new_value):
+		if type != new_value:
+			load_model(type_to_model_scene_name(new_value))
+			set_tint(group_to_color(group))
+		type = new_value
 
-@export var type: Types = Types.TOWER_NODE
-@export var group: HackingGroups = HackingGroups.NEUTRAL
+@export var group: StaticData.HackingGroups = StaticData.HackingGroups.NEUTRAL:
+	set(new_value):
+		if group != new_value:
+			set_tint(group_to_color(new_value))
+		group = new_value
 
 @export var hp: int = 8
 @export var hp_max: int = 8
@@ -13,11 +21,59 @@ enum HackingGroups { PINK, BLUE, NEUTRAL }
 @export var ap: int = 3
 @export var ap_max: int = 3
 
+var model = null
+var material = null
+
 func is_static() -> bool:
-	return type == Types.CENTRAL_NODE || type == Types.TOWER_NODE
+	return type == StaticData.UnitTypes.CENTRAL_NODE || type == StaticData.Types.TOWER_NODE
 
 func can_attack() -> bool:
 	return attack > 0 || attack_extra > 0
 
 func can_move() -> bool:
 	return !is_static() && ap > 0
+	
+func type_to_model_scene_name(which_type: StaticData.UnitTypes) -> String:
+	return StaticData.UnitTypes.keys()[which_type].to_lower()
+
+func group_to_color(which_group: StaticData.HackingGroups) -> Color:
+	if which_group == StaticData.HackingGroups.PINK:
+		return StaticData.color_pink
+	if which_group == StaticData.HackingGroups.BLUE:
+		return StaticData.color_blue
+	return StaticData.color_neutral
+
+func load_model(model_scene_name: String):
+	if model != null:
+		model.queue_free()
+	
+	model = load("res://art/" + model_scene_name + ".tscn").instantiate()
+	add_child(model)
+	
+	var mesh_instances = model.find_children("", "MeshInstance3D")
+	var mesh_instance = mesh_instances[-1]
+	material = StandardMaterial3D.new()
+	mesh_instance.material_override = material
+	
+	#var lights = model.find_children("", "Light3D")
+	#for light in lights:
+	#	light.queue_free()
+	
+	#var cameras = model.find_children("", "Camera3D")
+	#for camera in cameras:
+	#	camera.queue_free()
+	
+	pass
+	
+func set_tint(color: Color):
+	#print(material)
+	
+	if material != null:
+		material.albedo_color = color
+	pass
+
+func _ready():
+	#print(type_to_model_scene_name(type))
+	load_model(type_to_model_scene_name(type))
+	set_tint(group_to_color(group))
+	#type = Types.TROJAN
