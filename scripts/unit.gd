fc6cba6d250ae6c30e07a7e97dda9e962ed0307e
Igor <igorscorched@gmail.com>
2023-11-05 15:20:29 +0400

    Load unit stats from StaticData.

--- scripts/unit.gd
+++ scripts/unit.gd
@@ -4,8 +4,7 @@ extends Node3D
 @export var type: StaticData.UnitTypes = StaticData.UnitTypes.TOWER_NODE:
 	set(new_value):
 		if type != new_value:
-			load_model(type_to_model_scene_name(new_value))
-			set_tint(group_to_color(group))
+			type_changed_set_up(new_value)
 		type = new_value
 
 @export var group: StaticData.HackingGroups = StaticData.HackingGroups.NEUTRAL:
@@ -14,12 +13,12 @@ extends Node3D
 			set_tint(group_to_color(new_value))
 		group = new_value
 
-@export var hp: int = 8
-@export var hp_max: int = 8
-@export var attack: int = 0
-@export var attack_extra: int = 0
-@export var ap: int = 3
-@export var ap_max: int = 3
+var hp: int = 8
+var hp_max: int = 8
+var attack: int = 0
+var attack_extra: int = 0
+var ap: int = 3
+var ap_max: int = 3
 
 var model = null
 var material = null
@@ -63,7 +62,36 @@ func load_model(model_scene_name: String):
 	#for camera in cameras:
 	#	camera.queue_free()
 	
+	set_tint(group_to_color(group))
+	
 	pass
+	
+func load_stats(which_type: StaticData.UnitTypes):
+	var stats: Dictionary = StaticData.unit_stats[which_type]
+	
+	if stats.has("hp_max"):
+		hp_max = stats.hp_max
+		hp = hp_max
+	else:
+		hp_max = 1
+		hp = 1
+	
+	if stats.has("ap_max"):
+		ap_max = stats.ap_max
+		ap = ap_max
+	else:
+		ap_max = 0
+		ap = 0
+	
+	if stats.has("attack"):
+		attack = stats.attack
+	else:
+		attack = 0
+	
+	if stats.has("attack_extra"):
+		attack_extra = stats.attack_extra
+	else:
+		attack_extra = 0
 	
 func set_tint(color: Color):
 	#print(material)
@@ -72,8 +100,12 @@ func set_tint(color: Color):
 		material.albedo_color = color
 	pass
 
+func type_changed_set_up(new_type: StaticData.UnitTypes):
+	load_stats(new_type)
+	load_model(type_to_model_scene_name(new_type))
+
 func _ready():
 	#print(type_to_model_scene_name(type))
-	load_model(type_to_model_scene_name(type))
-	set_tint(group_to_color(group))
+	type_changed_set_up(type)
+	
 	#type = Types.TROJAN
