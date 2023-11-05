d4c6020576344b678a9e03e2e8dd1a25413f1807
Igor <igorscorched@gmail.com>
2023-11-05 15:30:34 +0400

    Fixed worm model modifiers and changed pink color to a better one.

--- scripts/static_data.gd
+++ scripts/static_data.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@export var color_pink: Color = Color.PINK
+@export var color_pink: Color = Color.DEEP_PINK
 @export var color_blue: Color = Color.BLUE
 @export var color_neutral: Color = Color.GRAY
 
